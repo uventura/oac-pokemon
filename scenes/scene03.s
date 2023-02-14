6258c944a37c4b573f31310e8988b405f6f3fa0a
Guala1 <gustavopvitena@gmail.com>
2023-02-14 00:20:55 -0300

    Gym adjust

--- scenes/scene03.s
+++ scenes/scene03.s
@@ -2,22 +2,24 @@ MAP_3:
 	.byte 
 	34,34,34,34,34,34,34,34,34,34,34,34,34,34,34,34,34,34,34,34,
 	33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,
-	38,118,118,118,36,36,36,36,38,36,36,38,36,36,36,36,118,118,118,38,
-	37,118,118,118,36,36,36,36,37,36,36,37,36,36,36,36,118,118,118,37,
-	118,118,118,38,118,118,36,36,36,36,36,36,36,36,118,118,38,118,118,118,
-	118,118,118,37,38,118,118,118,36,36,36,36,118,118,118,38,37,118,118,118,
-	118,118,36,36,37,36,36,36,36,36,36,36,36,36,36,37,36,36,118,118,
+	38,118,118,118,36,36,36,36,36,36,36,36,36,36,36,36,118,118,118,38,
+	37,118,118,118,36,36,36,36,36,36,36,36,36,36,36,36,118,118,118,37,
+	118,118,118,118,118,118,36,36,36,36,36,36,36,36,118,118,118,118,118,118,
+	118,118,118,118,118,118,118,118,36,36,36,36,118,118,118,118,118,118,118,118,
+	118,118,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,118,118,
 	118,118,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,118,118,
 	118,118,118,118,118,118,118,118,118,36,36,118,118,118,118,118,118,118,118,118,
 	118,118,118,118,118,118,118,118,118,36,36,118,118,118,118,118,118,118,118,118,
-	118,118,118,38,118,118,36,36,36,36,36,36,36,36,118,118,38,118,118,118,
-	36,36,36,37,36,36,36,36,36,36,36,36,36,36,36,36,37,36,36,36,
+	118,118,118,118,118,118,36,36,36,36,36,36,36,36,118,118,118,118,118,118,
 	36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,
-	36,36,36,36,36,36,36,36,38,36,36,38,36,36,36,36,36,36,36,36,
-	36,36,36,36,36,36,36,36,37,35,35,37,36,36,36,36,36,36,36,36,
+	36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,
+	36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,
+	36,36,36,36,36,36,36,36,36,35,35,36,36,36,36,36,36,36,36,36,
 	
 OBJECT_MAP_3:
-	.byte 14			        # Amount of objects
+	.byte 34			        # Amount of objects
+	# Bounderies
+	
 	# Windows 14
 	.byte 0, 1, 40, 0, 8		# Character Map => row, col, object, off_x, off_y
 	.byte 0, 2, 40, 0, 8		# Character Map => row, col, object, off_x, off_y
@@ -33,6 +35,31 @@ OBJECT_MAP_3:
 	.byte 0, 16, 40, 0, 8		# Character Map => row, col, object, off_x, off_y
 	.byte 0, 17, 39, 0, 8		# Character Map => row, col, object, off_x, off_y
 	.byte 0, 18, 40, 0, 8		# Character Map => row, col, object, off_x, off_y
+	# Statues 4  4  4  4  4
+	.byte 14, 8, 37, 0, 0		# Character Map => row, col, object, off_x, off_y
+	.byte 13, 8, 38, 0, 0		# Character Map => row, col, object, off_x, off_y
+	.byte 14, 11, 37, 0, 0		# Character Map => row, col, object, off_x, off_y
+	.byte 13, 11, 38, 0, 0		# Character Map => row, col, object, off_x, off_y
+
+	.byte 11, 0, 37, 0, 0		# Character Map => row, col, object, off_x, off_y
+	.byte 10, 0, 38, 0, 0		# Character Map => row, col, object, off_x, off_y
+	.byte 11, 19, 37, 0, 0		# Character Map => row, col, object, off_x, off_y
+	.byte 10, 19, 38, 0, 0		# Character Map => row, col, object, off_x, off_y
+
+	.byte 10, 6, 37, 0, 0		# Character Map => row, col, object, off_x, off_y
+	.byte 9, 6, 38, 0, 0		# Character Map => row, col, object, off_x, off_y
+	.byte 10, 13, 37, 0, 0		# Character Map => row, col, object, off_x, off_y
+	.byte 9, 13, 38, 0, 0		# Character Map => row, col, object, off_x, off_y
+
+	.byte 6, 2, 37, 0, 0		# Character Map => row, col, object, off_x, off_y
+	.byte 5, 2, 38, 0, 0		# Character Map => row, col, object, off_x, off_y
+	.byte 6, 17, 37, 0, 0		# Character Map => row, col, object, off_x, off_y
+	.byte 5, 17, 38, 0, 0		# Character Map => row, col, object, off_x, off_y
+
+	.byte 3, 8, 37, 0, 0		# Character Map => row, col, object, off_x, off_y
+	.byte 2, 8, 38, 0, 0		# Character Map => row, col, object, off_x, off_y
+	.byte 3, 11, 37, 0, 0		# Character Map => row, col, object, off_x, off_y
+	.byte 2, 11, 38, 0, 0		# Character Map => row, col, object, off_x, off_y
 
 LOCATION_CHANGE_3:
 	.byte 2				    		# Amount of locations
